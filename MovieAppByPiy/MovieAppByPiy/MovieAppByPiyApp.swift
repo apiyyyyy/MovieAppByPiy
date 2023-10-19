a79001b511564e8e2060fcc3f7ead3e7ce012499
apiyyyyy <afipermana13@live.com>
2023-10-19 11:48:11 +0700

    Add Init Xcode Project
    Add Init Xcode Project

---
+++ MovieAppByPiy/MovieAppByPiy/MovieAppByPiyApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieAppByPiyApp.swift
+//  MovieAppByPiy
+//
+//  Created by Afi Permana on 19/10/23.
+//
+
+import SwiftUI
+
+@main
+struct MovieAppByPiyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
